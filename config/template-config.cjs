b0a1206fa358bcb881d4d436e1383b807382f551
ハレーヒロ <51788278+HAERXILUO@users.noreply.github.com>
2023-07-25 16:23:52 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,9 +2,8 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
     desc: `
-      **{{date.DATA}}**
+      **🗓️{{date.DATA}}**
       
       下个休息日：{{holidaytts.DATA}}
       
@@ -15,16 +14,14 @@ const TEMPLATE_CONFIG = [
       天气：{{weather.DATA}}
       
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
+      紫外线强度指数☀️: {{tian_api_weather_uv_index_0.DATA}}
+      穿衣指数👚: {{tian_api_weather_tips_0.DATA}}
+            
       {{comprehensive_horoscope.DATA}}
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们在一起的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
@@ -32,9 +29,16 @@ const TEMPLATE_CONFIG = [
       
       {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      💌{{earthy_love_words.DATA}}
+
     `
   },
+  {
+
+
+
+  }
+
   {
     id: '0002',
     title: '推送完成提醒',
